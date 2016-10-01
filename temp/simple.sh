b6093332225d647272d2274921b90c34cc77eb15
Andy <andy.silv@yandex.ru>
2016-10-01 14:57:13 +0300

    script to init some work in rush repo

---
+++ temp/simple.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+rm -r .rush
+echo text > file
+./rush init
+./rush add file
+./rush commit -m "initial commit"
+echo "some more text" >> file
+./rush add file
+./rush commit -m "second commit"
+
